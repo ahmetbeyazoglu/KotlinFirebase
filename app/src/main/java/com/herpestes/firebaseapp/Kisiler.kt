0df06f1e6fe861ec7aa792304646626e424261b6
herpestes <ahmetbeyazoglu@gmail.com>
2023-08-23 21:19:55 +0300

    FirebaseConnect

---
+++ app/src/main/java/com/herpestes/firebaseapp/Kisiler.kt
@@ -0,0 +1,10 @@
+package com.herpestes.firebaseapp
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+
+@IgnoreExtraProperties
+data class Kisiler(var kisi_ad: String? = "", var kisi_yas:Int? ?=0) {
+
+
+}
